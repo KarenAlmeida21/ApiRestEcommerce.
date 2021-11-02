adde683122ccb0eaf2889bd22e62cc6cda26b0c9
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-01 23:31:13 -0300

    Criar método para cadastrar Lead na classe controller

--- Ecommerce/src/main/java/br/com/zup/Ecommerce/Lead/LeadController.java
+++ Ecommerce/src/main/java/br/com/zup/Ecommerce/Lead/LeadController.java
@@ -1,6 +1,8 @@
 package br.com.zup.Ecommerce.Lead;
 
+import br.com.zup.Ecommerce.DTO.LeadDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeadController {
     @Autowired
     private LeadService leadService;
+
+@PostMapping
+    public void cadastrarLead(LeadDTO leadDTO){
+        leadService.adicionarLead(leadDTO);
+    }
 
 }
