4327c4275a0167e503bfede3237d7d7cf333a959
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-01 21:55:51 -0300

    Criar Classe LeadDTO

---
+++ Ecommerce/src/main/java/br/com/zup/Ecommerce/LeadDTO.java
@@ -0,0 +1,50 @@
+package br.com.zup.Ecommerce;
+
+public class LeadDTO {
+    private String nome;
+    private String cpf;
+    private String email;
+    public ProdutoDTO produtoDTO;
+
+    public LeadDTO() {
+    }
+
+    public LeadDTO(String nome, String cpf, String email, ProdutoDTO produtoDTO) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.produtoDTO = produtoDTO;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ProdutoDTO getProdutoDTO() {
+        return produtoDTO;
+    }
+
+    public void setProdutoDTO(ProdutoDTO produtoDTO) {
+        this.produtoDTO = produtoDTO;
+    }
+}
